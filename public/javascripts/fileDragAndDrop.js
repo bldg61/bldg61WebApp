72531ba4284666f977901c56bf91b35843e3e949
Emily Platzer <emilyplatzer@gmail.com>
2019-11-11 14:34:25 -0700

    User can add multiple files at a time

--- public/javascripts/fileDragAndDrop.js
+++ public/javascripts/fileDragAndDrop.js
@@ -3,37 +3,45 @@ window.myCSVs = []
 
 async function dropHandler(ev) {
   ev.preventDefault();
+  let incomingFiles = []
 
   if (ev.dataTransfer.items) {
     for (var i = 0; i < ev.dataTransfer.items.length; i++) {
       if (ev.dataTransfer.items[i].kind === 'file') {
-        var file = ev.dataTransfer.items[i].getAsFile();
-
-        window.myCSVs.push(await file.text())
-
-        const newFileLI = document.createElement('li')
-        newFileLI.className = 'collection-item'
-        newFileLI.innerHTML = file.name
-
-        const addedFileList = document.getElementById('addedFileList')
-        addedFileList.append(newFileLI)
-
-        const numberFilesAdded = document.getElementById('numberFilesAdded')
-        numberFilesAdded.innerHTML = window.myCSVs.length === 1 ? `${window.myCSVs.length} file` : `${window.myCSVs.length} files`
-
-        const mungeButton = document.getElementById('mungeButton')
-        mungeButton.className = mungeButton.className.split('disabled').join('')
+        const file = ev.dataTransfer.items[i].getAsFile();
+        incomingFiles.push(file)
+        addFileToList(file)
       }
     }
   } else {
-    // Use DataTransfer interface to access the file(s)
     for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-      console.log("OH MY YOU ARE TRIGGERING THE ELSE STATEMENT...");
-      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+      const file = ev.dataTransfer.files[i]
+      incomingFiles.push(file)
+      addFileToList(file)
     }
   }
+
+  const readFiles = await Promise.all(incomingFiles.map(file => {
+    return file.text()
+  }))
+  window.myCSVs = [ ...window.myCSVs, ...readFiles]
+
+  const numberFilesAdded = document.getElementById('numberFilesAdded')
+  numberFilesAdded.innerHTML = window.myCSVs.length === 1 ? `${window.myCSVs.length} file` : `${window.myCSVs.length} files`
 }
 
 function dragOverHandler(ev) {
   ev.preventDefault();
+}
+
+function addFileToList(file) {
+  const newFileLI = document.createElement('li')
+  newFileLI.className = 'collection-item'
+  newFileLI.innerHTML = file.name
+
+  const addedFileList = document.getElementById('addedFileList')
+  addedFileList.append(newFileLI)
+
+  const mungeButton = document.getElementById('mungeButton')
+  mungeButton.className = mungeButton.className.split('disabled').join('')
 }
