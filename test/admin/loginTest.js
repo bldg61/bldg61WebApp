ee186a7f4f5e6264c39cb995bd173d8eadacb2f4
Emily Platzer <emilyplatzer@gmail.com>
2019-12-22 18:59:41 -0700

    Add selenium webdriver and example

--- test/admin/loginTest.js
+++ test/admin/loginTest.js
@@ -1,3 +1,4 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const request = require('supertest');
 const { expect } = require('chai');
 
@@ -15,5 +16,17 @@ describe('Admin path', async () => {
     expect(res.text).to.include('Email');
     expect(res.text).to.include('Password');
     expect(res.text).to.include('Login');
-  });
+  }).timeout(15000);;
+
+  it('...Selenium example...', async () => {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+      await driver.get('http://www.google.com/ncr');
+      await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+      await driver.wait(until.titleIs('webdriver - Google Search'), 3000);
+    } finally {
+      await driver.quit();
+      expect(true).to.equal(true)
+    }
+  }).timeout(15000);
 });
