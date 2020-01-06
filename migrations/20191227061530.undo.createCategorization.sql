a98433f6c2138d790cb8049b4ffe0ee27f3fc002
Emily Platzer <emilyplatzer@gmail.com>
2020-01-05 19:30:09 -0700

    Drop constraints for foreign keys

--- migrations/20191227061530.undo.createCategorization.sql
+++ migrations/20191227061530.undo.createCategorization.sql
@@ -1,7 +1,7 @@
--- ALTER TABLE "categorizations"
--- DROP CONSTRAINT categorizations_equipmentId_fkey;
---
--- ALTER TABLE "categorizations"
--- DROP CONSTRAINT categorizations_categoryId_fkey;
+ALTER TABLE "categorizations"
+DROP CONSTRAINT "categorizations_equipmentId_fkey";
+
+ALTER TABLE "categorizations"
+DROP CONSTRAINT "categorizations_categoryId_fkey";
 
 DROP TABLE IF EXISTS "categorizations";
