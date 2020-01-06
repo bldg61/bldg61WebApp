aa2315945584a0d3f575e57b81b182353096c3fe
Emily Platzer <emilyplatzer@gmail.com>
2020-01-05 19:22:28 -0700

    Abstract materialize initializations into own file

---
+++ public/javascripts/materializeScriptInitializations.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const tabs = document.querySelector('.tabs');
+  M.Tabs.init(tabs, {});
+  const modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals, {});
+});
