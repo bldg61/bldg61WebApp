add1b402c6920b118e3e0486501841c3aa67a3aa
Emily Platzer <emilyplatzer@gmail.com>
2020-01-30 10:10:45 -0700

    Admins get navigation to all restricted areas

--- routes/signupCompiler.js
+++ routes/signupCompiler.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const User = require('../models/user');
+
 router.get('/', async (req, res) => {
-  res.render('signupCompiler');
+  const currentUser = await User.find(req.session.userId);
+  res.render('signupCompiler', { currentUser });
 });
 
 module.exports = router;
