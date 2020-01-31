f812db64650916d045db1b3779947014e67ffd7c
Emily Platzer <emilyplatzer@gmail.com>
2020-01-30 17:16:30 -0700

    Move materialize initializations to layout, prep for possible pushpin migration from stickyheaders

--- public/javascripts/materializeScriptInitializations.js
+++ public/javascripts/materializeScriptInitializations.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const tabs = document.querySelector('.tabs');
-  M.Tabs.init(tabs, {});
   const modals = document.querySelectorAll('.modal');
   M.Modal.init(modals, {});
+  const pushpins = document.querySelectorAll('.pushpin');
+  M.Pushpin.init(pushpins, {});
+  const tabs = document.querySelector('.tabs');
+  M.Tabs.init(tabs, {});
   const selects = document.querySelectorAll('select');
   M.FormSelect.init(selects, {});
 });
