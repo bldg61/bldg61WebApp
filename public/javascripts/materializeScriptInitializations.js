30903df3b58853bbac05fe34276ed16f0ba49432
Emily Platzer <emilyplatzer@gmail.com>
2020-02-25 12:50:02 -0700

    Add sidebar for mobile admin nav links

--- public/javascripts/materializeScriptInitializations.js
+++ public/javascripts/materializeScriptInitializations.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const modals = document.querySelectorAll('.modal');
   M.Modal.init(modals, {});
-  const pushpins = document.querySelectorAll('.pushpin');
-  M.Pushpin.init(pushpins, {});
-  const tabs = document.querySelector('.tabs');
-  M.Tabs.init(tabs, {});
+
   const selects = document.querySelectorAll('select');
   M.FormSelect.init(selects, {});
+
+  const sidenavs = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenavs, {});
+
+  const tabs = document.querySelector('.tabs');
+  M.Tabs.init(tabs, {});
 });
